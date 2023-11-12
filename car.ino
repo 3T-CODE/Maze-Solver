c2c42b1a7e263f0736479479488e2550427dfa85
Bao-Trinh-Quoc <131318295+Bao-Trinh-Quoc@users.noreply.github.com>
2023-11-12 16:45:04 +0700

    Update car.ino
    Code tot hon

--- car.ino
+++ car.ino
@@ -1,3 +1,18 @@
+// kiem soat duoc cam bien ---- quan trong !
+// co hai van de chinh: dieu khien xe va giai thuat giai me cung
+// dieu khien xe thi sao? thi gom cac tac vu co ban nhu quay trai, quay phai, di thang quay dau nguoc lai va dung
+// giai thuat thi sao? phan giai thuat va implemented bang code 
+// 
+// cach xoay 90 do bang encoder?
+
+// khi dat xe o giua cach hai tuong 3cm thi cam bien tra ve no bi lech ... . --- do mcu
+// cam bien < 2cm thi phai co cach xu ly! 2cm - 4cm --- cho lui xe
+
+//wall distance???
+
+//====================x===================
+// lieu co the chay qua het me cung mot luot roi de ghi lai du lieu roi sau do moi cho no chay lai de toi uu ko?
+
 #include "carMazeOfOz.h"
 
 
@@ -8,12 +23,25 @@ volatile float speedValueLeft, speedValueRight;
 volatile float speedValueLeft_SAMPLE[11], speedValueRight_SAMPLE[11];
 volatile unsigned long timerPoint = 0, currentEncoderLeft = 0, currentEncoderRight = 0;
 
-// Your variables is in the area below
+// Your variables are in the area below
 //--------------------------------------------------//
+//int adjustedSpeed = 255;
+bool turnFinish = true;
+bool turnRight = false;
+byte setSpeedLeft = 80;
+byte setSpeedRight = 80; 
 
 
+//const float speedSetPoint = 70;
+const float distanceSetPoint = 9;
+const float rateSetPoint = 0;
 
+struct countPulse {
+  byte value = 0;
+  bool status = false;
+};
 
+volatile countPulse turn;
 
 //--------------------------------------------------//
 
@@ -23,8 +51,11 @@ void setup() {
   car.setInterrupt();
   attachInterrupt(LOW, ENC_LEFT_ISR, RISING);
   attachInterrupt(HIGH, ENC_RIGHT_ISR, RISING);
-}
 
+
+  
+}
+//purpose of this? read speed òf two motors?
 void ENC_LEFT_ISR() {
   static byte i = 0;
   speedValueLeft_SAMPLE[10] -= speedValueLeft_SAMPLE[i];
@@ -34,6 +65,10 @@ void ENC_LEFT_ISR() {
   car.setSpeedLeft(speedValueLeft);
   i = (i + 1) % 10;
   currentEncoderLeft = COUNTER_TIMER;
+  if (turn.status) {
+   turn.value++;
+  }
+
 }
 
 void ENC_RIGHT_ISR() {
@@ -45,85 +80,145 @@ void ENC_RIGHT_ISR() {
   car.setSpeedRight(speedValueRight);
   i = (i + 1) % 10;
   currentEncoderRight = COUNTER_TIMER;
+  if (turn.status) {
+    turn.value++;
+  }
 }
 
-// Your functions is in the area below
+// Your functions are in the area below
 //--------------------------------------------------//
 
+void speedLeftPID(float speedSetPoint ) {
+  float Kp = 1, Ki = 0.2;
+  float e = speedSetPoint - car.getSpeedLeft();
+  float P = Kp * e;
+  static float I = 0;
+  I += Ki * e;
+  setSpeedLeft = constrain(P + I, 0, 255);
+}
 
+void speedRightPID(float speedSetPoint) {
+  float Kp = 1, Ki = 0.2;
+  float e = speedSetPoint - car.getSpeedRight();
+  float P = Kp * e;
+  static float I = 0;
+  I += Ki * e;
+  setSpeedRight = constrain(P + I, 0, 255);
+}
 
+//void distancePID() {
+//  float Kp = 2, Ki = 0.25;
+//  float e = distanceSetPoint - car.getDistanceRight();
+//  float P = Kp * e;
+//  static float I = 0;
+//  if (e > -0.5 && e < 0.5) {
+//    I = 0;
+//  } else {
+//    I += Ki * e;
+//  }
+//  setDistance = constrain(P + I, -50, 50);
+//}
 
+//void rateOfChangeDistancePID() {
+//  float Kp = 0.1, Ki = 0.1;
+//  float e = rateSetPoint - car.getRateOfChangeDistanceLeft();
+//  float P = Kp * e;
+//  static float I = 0;
+//  if (e > -0.5 && e < 0.5) {
+//    I = 0;
+//  } else {
+//    I += Ki * e;
+//  }
+//  setRate = constrain(P + I, -50, 50);
+//}
 
 //--------------------------------------------------//
+
 
 void loop() {
   digitalWrite(trig, LOW);
   car.setSpeedLeft(speedValueLeft);
   car.setSpeedRight(speedValueRight);
-  car.configureSpeed(speedValueLeft, speedValueRight);
+  car.configureSpeed(speedValueLeft, speedValueRight);  //??
   
   // Your code is in the area below
   //--------------------------------------------------//
   //-----Example code-----//
-  // if (car.getDistanceHead() < 50) {
-  //   if (car.getDistanceHead() < 20) {
-  //     car.setMotorLeft(MAX_PWM, BACKWARD);
-  //     car.setMotorRight(MAX_PWM, FORWARD);
-  //   } else {
-  //     car.setMotorLeft(150, FORWARD);
-  //     car.setMotorRight(150, FORWARD);
-  //   }
-  // } else {
-  //   car.setMotorLeft(200, FORWARD);
-  //   car.setMotorRight(200, FORWARD);
-  // }
+  if (turnFinish) {
+    if (car.getDistanceRight() > 25) {
+      turn.status = true;
+      turnRight = true;
+    } else if (car.getDistanceHead() < 10){
+      turn.status = true;
+      turnRight = false;
+    }
+    if (turn.status) {
+      turnFinish = false;
+    }
+  }
+  if (turn.status) {
+    if (turnRight) {
+      speedLeftPID(40);
+      speedRightPID(40);
+      car.setMotorLeft(setSpeedLeft, 1);
+      car.setMotorRight(setSpeedRight, 0); 
+    } else {
+      speedLeftPID(40);
+      speedRightPID(40);
+      car.setMotorLeft(setSpeedLeft, 0);
+      car.setMotorRight(setSpeedRight, 1);
+    }
+  } else {
+    speedLeftPID(40);
+    speedRightPID(40);
+    car.setMotorLeft(setSpeedLeft, 1);
+    car.setMotorRight(setSpeedRight, 1);
+  }
+  if (turn.value > 45) {
+    turn.status = false;
+    turn.value = 0;
+    turnFinish = true;
+    if (car.getDistanceHead() > 10) {
+    speedLeftPID(40);
+    speedRightPID(40);
+    car.setMotorLeft(setSpeedLeft, 1);
+    car.setMotorRight(setSpeedRight, 1);
+    delay(550); //550
+    }
+  }
+
+  if (car.getDistanceHead()> 500) {
+    car.setMotorLeft(0, 0);
+    car.setMotorRight(0, 0);
+    delay(100);
+  }
+
+
+//  speedLeftPID();
+//  speedRightPID();
+//  car.setMotorLeft(setSpeedLeft, 1);
+//  car.setMotorRight(setSpeedRight, 1);
   
-  // Use Serial Monitor or Plotter in Arduino IDE to display the values on the screen
-  // Serial.print(car.getDistanceHead());
-  // Serial.print(" ");
-  // Serial.print(car.getSpeedLeft());
-  // Serial.print(" ");
-  // Serial.println(car.getSpeedRight());
+
+  
+     
+//  // Use Serial Monitor or Plotter in Arduino IDE to display the values on the screen
+//  Serial.print(car.getDistanceHead());
+//  Serial.print(" ");
+//  Serial.print(car.getSpeedLeft());
+//////  Serial.print(car.getDistanceLeft());
+//  Serial.print(" ");
+//  Serial.println(car.getSpeedRight());
+//  Serial.print(" ");
+//  Serial.println(car.getDistanceRight());
   //-----Example code-----//
-  
-  
+
+  //Righ-hand rule
+  //always move forward, when detects obstacle --> always turn right
+  //then move forward. when detects obstacle ahead and right, turn left
+  // if detects obstacle ahead, right, left, turn around 180
   
   
   
   //--------------------------------------------------//
 }
-
-
-
-
-//----------------------------------------------------//
-//--------------------MOVE_FUNCTION-------------------//
-//----------------------------------------------------//
-int TurnLeft(int speed)
-{
-    car.setMotorLeft(speed,BACKWARD);
-    car.setMotorRight(speed,FORWARD);
-}
-
-int TurnRight(int speed)
-{
-    car.setMotorRight(speed,BACKWARD);
-    car.setMotorLeft(speed,FORWARD); 
-}
-
-int MoveForward(int speed)
-{
-    car.setMotorLeft( speed,FORWARD);
-    car.setMotorRight(speed,FORWARD);
-}
-
-int MoveBackward(int speed)
-{
-    car.setMotorLeft(speed, BACKWARD);
-    car.setMotorRight(speed,BACKWARD);
-
-}
-
-//----------------------------------------------------//
-
-
